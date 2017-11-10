3d0c9f6414e16464f2991a92d6b49b57a631d868
Patrice Juergens <pj@validitylabs.org>
2017-11-10 18:55:17 +0100

    🔧 Update test-runner script

--- tools/test-runner.js
+++ tools/test-runner.js
@@ -10,16 +10,46 @@ const Ganache = require('ganache-core');
 const port      = 9545;
 const config    = {
     accounts: [
-        {balance: 'F4240'},
-        {balance: 'F4240'},
-        {balance: 'F4240'},
-        {balance: 'F4240'},
-        {balance: 'F4240'},
-        {balance: 'F4240'},
-        {balance: 'F4240'},
-        {balance: 'F4240'},
-        {balance: 'F4240'},
-        {balance: 'F4240'}
+        {
+            balance: '0x7FFFFFFFFFFFFFFF',
+            secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200'
+        },
+        {
+            balance: '0x7FFFFFFFFFFFFFFF',
+            secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501201'
+        },
+        {
+            balance: '0x7FFFFFFFFFFFFFFF',
+            secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501202'
+        },
+        {
+            balance: '0x7FFFFFFFFFFFFFFF',
+            secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501203'
+        },
+        {
+            balance: '0x7FFFFFFFFFFFFFFF',
+            secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501204'
+        },
+        {
+            balance: '0x7FFFFFFFFFFFFFFF',
+            secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501205'
+        },
+        {
+            balance: '0x7FFFFFFFFFFFFFFF',
+            secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501206'
+        },
+        {
+            balance: '0x7FFFFFFFFFFFFFFF',
+            secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501207'
+        },
+        {
+            balance: '0x7FFFFFFFFFFFFFFF',
+            secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501208'
+        },
+        {
+            balance: '0x7FFFFFFFFFFFFFFF',
+            secretKey: '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501209'
+        }
     ],
     mnemonic: 'waste system voyage dentist fine donate purpose truly cactus chest coyote globe',
     port: 9545,
