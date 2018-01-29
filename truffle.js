6f899715a2351a1287494d08867e2a57e9dda33c
Patrice Juergens <pj@validitylabs.org>
2018-01-29 09:45:25 +0100

    🔧 Re add gas price for mainnet and rinkeby networks

--- truffle.js
+++ truffle.js
@@ -29,12 +29,12 @@ module.exports = {
         _development: {
             host: 'localhost',
             port: 9545,
-            network_id: 4447,
+            network_id: 4447, // eslint-disable-line
             gas: cnf.network.rinkeby.gas
         },
         coverage: {
             host:       'localhost',
-            network_id: 4447,
+            network_id: 4447, // eslint-disable-line
             port:       8555,
             gasPrice:   1,
             gas:        100000000
@@ -42,21 +42,23 @@ module.exports = {
         rinkeby: {
             host:       cnf.network.rinkeby.host,
             from:       cnf.network.rinkeby.from,
-            network_id: 4,
+            network_id: 4, // eslint-disable-line
             port:       8545,
+            gasPrice:   cnf.network.rinkeby.gasPrice,
             gas:        cnf.network.rinkeby.gas
         },
         mainnet: {
             host:       cnf.network.mainnet.host,
             from:       cnf.network.mainnet.from,
-            network_id: 1,
+            network_id: 1, // eslint-disable-line
             port:       8545,
+            gasPrice:   cnf.network.mainnet.gasPrice,
             gas:        cnf.network.mainnet.gas
         }
     },
-    build_directory:            buildDir,
-    contracts_build_directory:  buildDirContracts,
-    migrations_directory:       migrationsDir,
-    contracts_directory:        srcDir,
-    test_directory:             testDir
+    build_directory:            buildDir,           // eslint-disable-line
+    contracts_build_directory:  buildDirContracts,  // eslint-disable-line
+    migrations_directory:       migrationsDir,      // eslint-disable-line
+    contracts_directory:        srcDir,             // eslint-disable-line
+    test_directory:             testDir             // eslint-disable-line
 };
