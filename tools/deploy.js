f0d053cca40438fc0948a9568bbd357f18d9482d
Patrice Juergens <pj@validitylabs.org>
2018-01-30 11:54:34 +0100

    🔧🎨 Prepare MainNet deployment and cleanup the code

--- tools/deploy.js
+++ tools/deploy.js
@@ -8,20 +8,15 @@ require('babel-polyfill');
 import {logger as log} from './logger';
 import cnf from '../ico.cnf.json';
 import Web3 from 'web3';
-import * as icoCrowdsaleModule from '../build/mew/IcoCrowdsale.sol.js';
+import * as icoCrowdsaleModule from '../build/bundle/IcoCrowdsale.sol.js';
 
 const provider      = `http://${cnf.network[process.env.NODE_ENV].host}:${cnf.network[process.env.NODE_ENV].port}`;
 const web3          = new Web3(new Web3.providers.HttpProvider(provider));
 const abi           = icoCrowdsaleModule.IcoCrowdsaleAbi;
 const bin           = icoCrowdsaleModule.IcoCrowdsaleByteCode;
 
-async function getAccounts() {
-    const ret = await web3.eth.getAccounts();
-    return ret;
-}
-
 async function deploy() {
-    const accounts  = await getAccounts();
+    let from        = null;
     let wallet      = null;
     let underwriter = null;
     let startTime   = null;
@@ -30,10 +25,11 @@ async function deploy() {
     const rateChfPerEth         = cnf.rateChfPerEth;
     const confirmationPeriod    = cnf.confirmationPeriod;
 
+    log.info(`[ Deploying on ${process.env.NODE_ENV} ]`);
+
     switch (process.env.NODE_ENV) {
         case 'rinkeby':
-            log.info('[ Deploying on Rinkeby ]');
-
+            from        = cnf.network.rinkeby.from;
             wallet      = cnf.network.rinkeby.wallet;
             underwriter = cnf.network.rinkeby.underwriter;
             startTime   = cnf.startTime;
@@ -44,7 +40,7 @@ async function deploy() {
                 null,
                 {
                     data:       bin,
-                    from:       accounts[0],
+                    from:       from,
                     gas:        cnf.network.rinkeby.gas,
                     gasPrice:   cnf.network.rinkeby.gasPrice
                 }
@@ -63,42 +59,32 @@ async function deploy() {
             }).send({
                 gas:        cnf.network.rinkeby.gas,
                 gasPrice:   cnf.network.rinkeby.gasPrice,
-                from: accounts[0]
-            }, (error, transactionHash) => {
-                if (error) {
-                    log.error(error);
-                }
-                // else {
-                //     log.info(`Transaction hash: ${transactionHash}`);
-                // }
+                from: from
             }).on('error', (error) => {
-                log.error('Error occured');
+                log.error('Error occured:');
                 log.error(error);
             }).on('transactionHash', (transactionHash) => {
-                log.info(`Transaction hash: ${transactionHash}`);
-            }).on('receipt', (receipt) => {
-                log.info('Contract receipt (contains the new contract address)');
-                log.info(receipt.contractAddress);
-            })
-            // .on('confirmation', (confirmationNumber, receipt) => {
-            //     log.info(`confirmationNumber: ${confirmationNumber}`);
-            //     log.info('receipt:');
-            //     log.info(receipt);
-            // })
-            .then((newContractInstance) => {
-                log.info(`Address of new instance: ${newContractInstance.options.address}`);
+                log.info(`Your contract is being deployed in transaction at http://rinkeby.etherscan.io/tx/${transactionHash}`);
+            }).then((newContractInstance) => {
+                log.info(`Smart contract address on Etherscan is https://rinkeby.etherscan.io/address/${newContractInstance.options.address}`);
             }).catch((error) => {
-                log.error('Exception thrown');
+                log.error('Exception thrown:');
                 log.error(error);
             });
 
-            icoCrowdsaleContract.options.address = icoCrowdsaleInstance.options.address;
+            // Debug:
+            // icoCrowdsaleInstance.on('receipt', (receipt) => {
+            //     log.info('Contract receipt (contains the new contract address)');
+            //     log.info(receipt.contractAddress);
+            // }).on('confirmation', (confirmationNumber, receipt) => {
+            //     log.info(`confirmationNumber: ${confirmationNumber}`);
+            //     log.info('receipt:');
+            //     log.info(receipt);
+            // });
 
-            log.info(`icoCrowdsaleContractAddress: ${icoCrowdsaleContract.options.address}`);
-            log.info(`Your contract is being deployed in transaction at http://rinkeby.etherscan.io/tx/${icoCrowdsale.transactionHash}`);
+            icoCrowdsaleContract.options.address = icoCrowdsaleInstance.options.address;
             break;
         case 'mainnet':
-            log.info('[ Deploying on MainNet ]');
             log.error('Not implemented yet');
             break;
         default:
