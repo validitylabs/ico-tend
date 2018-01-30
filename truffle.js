970ef52711f7145102464ae8fe738a5b53d5e127
Patrice Juergens <pj@validitylabs.org>
2018-01-30 12:45:56 +0100

    🔧💣 Remove unneeded truffle configuration and NPM run scripts. Please consider the README!

--- truffle.js
+++ truffle.js
@@ -26,34 +26,12 @@ module.exports = {
         }
     },
     networks: {
-        _development: {
-            host: 'localhost',
-            port: 9545,
-            network_id: 4447, // eslint-disable-line
-            gas: cnf.network.rinkeby.gas
-        },
         coverage: {
             host:       'localhost',
             network_id: 4447, // eslint-disable-line
             port:       8555,
             gasPrice:   1,
             gas:        100000000
-        },
-        rinkeby: {
-            host:       cnf.network.rinkeby.host,
-            from:       cnf.network.rinkeby.from,
-            network_id: 4, // eslint-disable-line
-            port:       cnf.network.rinkeby.port,
-            gasPrice:   cnf.network.rinkeby.gasPrice,
-            gas:        cnf.network.rinkeby.gas
-        },
-        mainnet: {
-            host:       cnf.network.mainnet.host,
-            from:       cnf.network.mainnet.from,
-            network_id: 1, // eslint-disable-line
-            port:       cnf.network.mainnet.port,
-            gasPrice:   cnf.network.mainnet.gasPrice,
-            gas:        cnf.network.mainnet.gas
         }
     },
     build_directory:            buildDir,           // eslint-disable-line
